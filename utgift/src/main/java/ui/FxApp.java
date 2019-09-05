7d43527a67819203e621f8c05dd9edf3407296bb
Lotfi_laptop <fijgobal99@gmail.com>
2019-09-05 12:12:53 +0200

    fxApp og fxApfxml

---
+++ utgift/src/main/java/ui/FxApp.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class FxApp extends Application{
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Utgift");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("FxApp.fxml"))));
+        primaryStage.show();
+
+    }
+    public static void main(final String[] args) {
+        Application.launch(args);
+    }
+}
