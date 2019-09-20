58066b2ac2aebe3e0ef4f3f233ae302bdcd90fb1
Nicolaad <dalaaker.nicolai@gmail.com>
2019-09-20 12:12:03 +0200

    Completed json saving and loading + changed core structure to be based on Utgift objects instead of strings for ease of use

--- utgift/src/main/java/json/Load.java
+++ utgift/src/main/java/json/Load.java
@@ -1,37 +1,32 @@
 package json;
 
 import core.Utgift;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Observable;
-
 import com.google.gson.Gson;
 
 public class Load {
-    //retrieves stored utgift objects from a file and returns an arrayList
+    //retrieves stored utgift objects from a json array in a json file.
     public static Collection<Utgift> retrieve(File file){
-        Collection<Utgift> utgifter = new ArrayList<Utgift>();
+        Collection utgifter = new ArrayList<Utgift>();
 
         try {
             Gson gson = new Gson();
             Reader reader = new FileReader(file);
-            utgifter = gson.fromJson(reader, ArrayList.class);
+
+            Utgift[] array = gson.fromJson(reader, Utgift[].class);
             reader.close();
+
+            for (Utgift u : array){
+                utgifter.add(u);
+            }
         }
         catch(IOException e){
             System.out.println(e);
         }
-        objectify(utgifter);
+
         return(utgifter);
     }
 
-    private static void objectify(Collection utgifterJsoned){
-        Collection cleanedCollection = new ArrayList<>();
-        for (Object u : utgifterJsoned){
-            System.out.println(u);
-        }
-        //return cleanedCollection;
-    }
 }
--- utgift/src/main/java/json/Save.java
+++ utgift/src/main/java/json/Save.java
@@ -1,18 +1,15 @@
 package json;
 
 import core.Utgift;
-
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Collection;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-
-//allows saving and retrieving - will be superseded by json at a later point
+//allows saving to json files.
 public class Save {
 
-    //writes all objects in an array list to a file
+    //writes a utgift collection as a json object to a file.
     public static void save(Collection<Utgift> utgifter, File file){
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
