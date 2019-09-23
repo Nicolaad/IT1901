4fcbe0a643b5d01594da201f786f9c4d9cdc1baf
Nicolaad <dalaaker.nicolai@gmail.com>
2019-09-23 13:20:46 +0200

    la til save og load tester som er optimalisert for json. Fiksa utdaterte tester. NB trenger å covre exceptions -- hvordan??

--- utgift/src/test/java/json/LoadSaveTest.java
+++ utgift/src/test/java/json/LoadSaveTest.java
@@ -5,12 +5,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import static junit.framework.Assert.fail;
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
 
 public class LoadSaveTest {
     private Collection<Utgift> utgifter = new ArrayList<>();
@@ -23,25 +25,29 @@ public class LoadSaveTest {
         utgifter.add(new Utgift("Flytogbusstogfly", "200000", "Skole"));
         file = new File("test.json");
 
-    /*
-    @Before
-    public void init(){
-        utgifter.add(new Utgift("hei","hei","heilo").toString());
-        utgifter.add(new Utgift("hei","hei","hei").toString());
-        file = new File("test.txt");
-
     }
-    */
-        @Test
-        public void saveTest() {
-            //test that it can store and retrieve proper objects
-            json.Save.save(utgifter,file);
-            Collection<Utgift> retrievedUtgifter = json.Load.retrieve(file);
-            assertTrue(trueutgifter.equals(retrievedUtgifter))){
-                assertTrue();
-            }
+    @Test
+    public void saveAndLoadIsEqual(){
+        //test that it can store and retrieve proper objects
+        json.Save.save(utgifter,file);
+        Collection<Utgift> retrievedUtgifter = json.Load.retrieve(file);
+
+        assertEquals(utgifter.toString(), retrievedUtgifter.toString());
         }
 
+    //sjekker om koden greier å handle at feil fil er spesifisert
+    @Test()
+    public void nonexistingFileLoadHandled() {
+        Collection<Utgift> retrievedUtgifter = json.Load.retrieve(new File("nonexistingFile.onenote"));
+        assert (true);
+    }
+    //sjekker om koden greier å handle at feil fil er spesifisert
+    @Test()
+    public void nonexistingFileSaveHandeled(){
+        Collection<Utgift> retrievedUtgifter = json.Load.retrieve(new File("nonexistingFile2.onenote"));
+        assert(true);
 
     }
+
 }
+
