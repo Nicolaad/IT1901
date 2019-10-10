07f205c6b17b7f16838d6cf8679636049da8266b
Lotfi <lotfial@stud.ntnu.no>
2019-10-10 11:33:20 +0200

    removed some comments and fixed utgiftlistmodule

--- utgift/fxui/src/main/java/ui/FxApp.java
+++ utgift/fxui/src/main/java/ui/FxApp.java
@@ -14,10 +14,10 @@ public class FxApp extends Application{
     public void start(final Stage primaryStage) throws Exception {
         primaryStage.setTitle("Utgift");
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("FxApp.fxml"))));
-        //rest.server.UtgiftListGrizzlyApp a = new rest.server.UtgiftListGrizzlyApp();
+        UtgiftListGrizzlyApp a = new UtgiftListGrizzlyApp();
         String []strings = {};
         primaryStage.show();
-        UtgiftListGrizzlyApp.main(strings);
+        UtgiftListGrizzlyApp.startServer(strings,0);
     }
 
     public static void main(final String[] args) {
