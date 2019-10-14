740bbf9b1b63d7f3ec8e6b0a883116e586983268
Nicolaad <dalaaker.nicolai@gmail.com>
2019-10-14 11:34:17 +0200

    added tests to serializers and deserializers

--- utgift/core/src/test/java/json/UtgiftListSerializingTest.java
+++ utgift/core/src/test/java/json/UtgiftListSerializingTest.java
@@ -9,45 +9,58 @@ import org.junit.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 
 public class UtgiftListSerializingTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();{
         objectMapper.registerModule(new UtgiftListModule());
     }
-
+    //hjelpemetoder
     private Utgift utgift1(){
         return new Utgift("ost", "53", "mat");
-        }
-
+    }
     private Utgift utgift2(){
         return new Utgift("bok", "1621", "skole");
     }
-    private void assertEqualsNoWhiteSpace(String input, String expected){
-        Assert.assertEquals(input.replaceAll("\\s+", ""), expected);
-    }
-    /*
+
+    /**
+     * Tester at objektmapperen gir riktig output når den konverterer flere objekter til en jsonstreng
+     * @throws JsonProcessingException
+     */
     @Test
     public void testUtgiftListSerialization() throws JsonProcessingException {
-        Collection<Utgift> initiadedUtgifter = new ArrayList<>();
-        initiadedUtgifter.add(utgift1());
-        initiadedUtgifter.add(utgift2());
-        String inputJson = objectMapper.writeValueAsString(new UtgiftList(initiadedUtgifter));
-        String expectedJson = "[{\"navn\":\"ost\",\"pris\":\"53\",\"kategori\":\"Mat\"}," + "{\"navn\":\"Akademikabok\",\"pris\":\"1621\",\"kategori\":\"Skole\"}]";
-        Assert.assertEquals(inputJson,expectedJson);
+
+        String inputJson = objectMapper.writeValueAsString(new UtgiftList(utgift1(),utgift2()));
+        String expectedJson = "[{\"navn\":\"ost\",\"pris\":53.0,\"kategori\":\"mat\",\"prisString\":\"53\"},{\"navn\":\"bok\",\"pris\":1621.0,\"kategori\":\"skole\",\"prisString\":\"1621\"}]";
+        Assert.assertEquals("compared:"+ inputJson + " with:" + expectedJson,inputJson,expectedJson);
         }
-    /*
+
+    /**
+     * Tester om den kan parse en json string med 2 utgift objekter som objekter, og se om deres strengverdi er lik tostringen til 2 vanlig instansierte utgiftobjekter.
+     * @throws Exception
+     */
     @Test
-    public void testUtgiftListDeserialization() throws Exception {
-        String json = "[{\"navn\":\"ost\",\"pris\":\"53\",\"kategori\":\"mat\"}," + "{\"navn\":\"bok\",\"pris\":\"1621\",\"kategori\":\"skole\"}]";
-        Collection ul = objectMapper.readValue(json, UtgiftList.class).getUtgifter();
-        //Assert.assertEquals(utgift1().toString(), utgiftList.toList().get(0).toString());
-        //Assert.assertEquals(utgift2(), utgiftList.toList().get(1).toString());
-        Assert.assertEquals(2, ul.size());
+    public void testUtgiftListDeserialization() throws IOException {
+        String json = "[{\"navn\":\"ost\",\"pris\":\"53\",\"kategori\":\"mat\"},{\"navn\":\"bok\",\"pris\":\"1621\",\"kategori\":\"skole\"}]";
+        Object[] ul = objectMapper.readValue(json, UtgiftList.class).getUtgifter().toArray();
+        Assert.assertEquals(ul[0].toString(), utgift1().toString(), ul[0].toString());
+        Assert.assertEquals(ul[1].toString(), utgift2().toString(), ul[1].toString());
+        Assert.assertEquals("Actuall length is " + ul.length, 2, ul.length);
 
     }
+    @Test
+    public void testUtgiftSerialization() throws JsonProcessingException{
+        String inputJson = objectMapper.writeValueAsString(new UtgiftList(utgift1()));
+        String expectedJson = "[{\"navn\":\"ost\",\"pris\":53.0,\"kategori\":\"mat\",\"prisString\":\"53\"}]";
+        Assert.assertEquals("compared:"+ inputJson + " with:" + expectedJson,inputJson,expectedJson);
+    }
     /*
+    @Test
+    public void testUtgiftDeserialization() throws IOException {
+        String json = "[{\"navn\":\"ost\",\"pris\":\"53\",\"kategori\":\"mat\"}]";
+        Utgift u = objectMapper.readValue(json, Utgift.class);
+        Assert.assertEquals("compares:"+ u+ " with:"+ utgift1(),utgift1().toString(), u.toString());
+
+    }
      */
 }
