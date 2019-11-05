f5d1085dc8081e6cc27d255fd72390502c283f3b
Lotfi Amin Lazreg <lotfial@stud.ntnu.no>
2019-11-05 16:08:11 +0100

    fixes #106 fikse restservertest

--- utgift/restserver/src/test/java/rest/server/UtgiftListGrizzlyAppTest.java
+++ utgift/restserver/src/test/java/rest/server/UtgiftListGrizzlyAppTest.java
@@ -1,9 +1,30 @@
 package rest.server;
 
-import static org.junit.Assert.*;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
 
 public class UtgiftListGrizzlyAppTest {
-    public void test(){
+    private HttpServer server;
 
+    @Before
+    public void setUp() throws Exception {
+        server = UtgiftListGrizzlyApp.startServer(null,-1);
     }
+
+    @After
+    public void tearDown() throws Exception {
+        UtgiftListGrizzlyApp.stopServer(server);
+    }
+
+    @Test
+    public void startStopServer() throws IOException {
+        UtgiftListGrizzlyApp.stopServer(server);
+        HttpServer s = UtgiftListGrizzlyApp.startServer(null,2);
+        UtgiftListGrizzlyApp.stopServer(s);
+    }
+
 }
